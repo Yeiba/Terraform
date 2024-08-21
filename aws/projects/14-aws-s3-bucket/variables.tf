94a98cb60f2c5d28e1aa904962b49b99f6bb51ea
Jacob_Belhardi <yaakoubbelhardi@gmail.com>
2024-08-21 07:54:55 +0100

    docker compose labe

---
+++ aws/projects/14-aws-s3-bucket/variables.tf
@@ -0,0 +1,26 @@
+
+# Tags
+variable "project" {}
+variable "prefix" {}
+variable "suffix" {}
+variable "createdby" {}
+variable "org_unit" {}
+variable "business_unit" {}
+variable "cost_center" {}
+variable "appid" {}
+
+# General 
+variable "aws_region" {}
+variable "aws_profile" {}
+
+# S3
+variable "s3_bucket_names" {}
+variable "s3_bucket_name" {}
+variable "s3_versioning" {}
+variable "enable_lifecycle_rule" {}
+
+variable "db_table_name" {}
+variable "billing_mode" {}
+variable "hash_key" {}
+variable "attr_name" {}
+variable "attr_type" {}
---
+++ aws/projects/14-aws-s3-bucket/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.37.0"
+    }
+  }
+  # backend "s3" {}
+}
+provider "aws" {
+  profile = var.aws_profile
+  region = var.aws_region
+}
