3b6c2c11f46be9a48d70df21a3d93f8f3a48943d
Jacob_Belhardi <yaakoubbelhardi@gmail.com>
2024-08-21 06:57:23 +0100

    remove backend.tfvars

--- aws/projects/00-0-aws-s3backend/main.tf
+++ aws/projects/00-0-aws-s3backend/main.tf
@@ -13,27 +13,3 @@ locals {
     environment  = terraform.workspace
   }
 }
-
-module "aws_s3_bucket" {
-  source = "./modules/aws-s3"
-
-  #for_each              = toset(var.s3_bucket_names)
-  #s3_bucket_name           = each.key
-
-  s3_bucket_name        = var.s3_bucket_name
-  enable_lifecycle_rule = var.enable_lifecycle_rule
-  s3_versioning         = var.s3_versioning
-  tags                  = merge({ "ResourceName" = var.s3_bucket_name }, local.tags)
-
-}
-
-module "aws_dynamodb" {
-  source        = "./modules/aws-dynamodb"
-  db_table_name = var.db_table_name
-  billing_mode  = var.billing_mode
-  hash_key      = var.hash_key
-  attr_name     = var.attr_name
-  attr_type     = var.attr_type
-  tags          = merge({ "ResourceName" = var.db_table_name }, local.tags)
-
-}
