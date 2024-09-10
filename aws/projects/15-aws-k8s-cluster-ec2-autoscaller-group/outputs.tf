b398d208be38b72e8e435b948a2ab390a8b17cb0
Jacob_Belhardi <yaakoubbelhardi@gmail.com>
2024-09-10 18:27:04 +0100

    add variables

--- aws/projects/15-aws-k8s-cluster-ec2-autoscaller-group/outputs.tf
+++ aws/projects/15-aws-k8s-cluster-ec2-autoscaller-group/outputs.tf
@@ -1,4 +1,4 @@
-output "external_nginx_lb_dns" {
-  description = "The DNS name of the external NGINX load balancer"
+output "external_nginx_lbc_dns" {
+  description = "The DNS name of the external NGINX load balancer controller"
   value       = aws_lb.k8s_alb.dns_name
 }
