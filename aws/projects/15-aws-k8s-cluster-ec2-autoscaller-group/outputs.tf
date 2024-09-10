bea2cc2949cf5a46b26282b4eaf82925ebaec200
Jacob_Belhardi <yaakoubbelhardi@gmail.com>
2024-09-10 18:26:50 +0100

    add variables

--- aws/projects/15-aws-k8s-cluster-ec2-autoscaller-group/outputs.tf
+++ aws/projects/15-aws-k8s-cluster-ec2-autoscaller-group/outputs.tf
@@ -1,3 +1,4 @@
-output "nginx_ingress_lb_dns" {
-  value = helm_release.nginx_ingress.status.load_balancer.ingress[0].hostname
+output "external_nginx_lb_dns" {
+  description = "The DNS name of the external NGINX load balancer"
+  value       = aws_lb.k8s_alb.dns_name
 }
